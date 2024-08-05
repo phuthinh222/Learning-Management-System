5e07b840a23bc044a22b8e72a8e8ba0b45be487b
khuyenphamno0 <khuyenphamno0>
2024-08-05 11:18:24 +0700

    Add comment in regex statements

--- app/Http/Requests/Authentication/RegisterRequest.php
+++ app/Http/Requests/Authentication/RegisterRequest.php
@@ -22,24 +22,37 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
+            //This following regex rule means:
+            // - Allow client to type Vietnamese name with Vietnamese characters.
+            // - Each word is separated by 1 space ' ' 
             'name' => ['required', 'regex: /^[A-Za-zÀ-ỹà-ỹ]+(?:\s[A-Za-zÀ-ỹà-ỹ]+)*$/'],
             'email_address' => [
                 'required', 
+                //The regex statement mentioned below will allow client to type an correct email address:
+                // - befor '@' character allow 3-64 characters with a-z or A-Z or 0-9 or some special character listed below.
+                // - After '@' character, we allow client to type domain at least 2 characters with a-z or A-Z or 0-9 and at least 1 dot('.')
+                // - Between dots ('.'), client must type at least 2 characters with a-z or A-Z or 0-9
                 'regex:/^[a-zA-Z0-9_\-\*\!\#\%\&\'\*\+\^]{3,64}@[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,}){1,}$/', 
                 'unique:users'
             ],
             'user_name' => [
                 'required',
+                //This Regex statement means: Client can type at least 3 and maximum 64 characters with a-z or A-Z or 0-9 or some spacial characters listed below.
                 'regex:/^[a-zA-Z0-9_\-\*\!\#\%\&\'\*\+\^]{3,64}/',
                 'unique:users' 
             ],
             'password' => [
                 'required',
+                //This regex allows client to type a strong password with some following rules:
+                // - At least 6 characters and maximum 64 characters
+                // - At least 1 uppercase character and at least 1 of some special characters mentioned below
+                // - At least 1 numberic character
                 'regex:/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d\!\@\#\$\%\^\&\*\(\_\\\.\<\>\;\:\'\"\-]{6,64}$/'
             ],
             'repeat_password' => [
                 'required',
                 'same:password',
+                //Same rule with password.
                 'regex:/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d\!\@\#\$\%\^\&\*\(\_\\\.\<\>\;\:\'\"\-]{6,64}$/'
             ],
         ];
