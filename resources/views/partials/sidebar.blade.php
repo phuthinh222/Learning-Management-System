ee8f90991e4f431026cabaae3b28dbebdab1c493
quocle300903 <quocle300903@gmail.com>
2024-08-08 14:55:47 +0700

    done create layout and feature logout

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,36 @@
+      <!-- Sidebar -->
+      <div class="sidebar" data-background-color="dark">
+          <div class="sidebar-logo">
+              <!-- Logo Header -->
+              <div class="logo-header " data-background-color="light">
+                  <a href="/" class="logo">
+                      <img src="assets/img/logo-ECM01.png" alt="navbar brand " class="navbar-brand"  height="150" />
+                  </a>
+                  <div class="nav-toggle">
+                      <button class="btn btn-toggle toggle-sidebar">
+                          <i class="gg-menu-right"></i>
+                      </button>
+                      <button class="btn btn-toggle sidenav-toggler">
+                          <i class="gg-menu-left"></i>
+                      </button>
+                  </div>
+                  <button class="topbar-toggler more">
+                      <i class="gg-more-vertical-alt"></i>
+                  </button>
+              </div>
+              <!-- End Logo Header -->
+          </div>
+          <div class="sidebar-wrapper scrollbar scrollbar-inner">
+              <div class="sidebar-content">
+                  <ul class="nav nav-secondary">
+                      <li class="nav-item">
+                          <a href="#" class="dashboard">
+                              <i class="fas fa-home"></i>
+                              <p>Dashboard</p>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <!-- End Sidebar -->
