343e2ea71ac1535a144dfafd3bcaf3e857183638
nguyen xuan sinh <nguyenxuansinh09876@gmail.com>
2024-08-19 14:46:09 +0700

    update layout information student

--- app/Http/Service/User/UserService.php
+++ app/Http/Service/User/UserService.php
@@ -73,4 +73,6 @@ class UserService
     {
         return substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / strlen($x)))), 1, $length);
     }
+
+    
 }
